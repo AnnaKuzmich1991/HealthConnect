28269d1b3b2e9564026cf9498c694b6e40302058
AnnaKuzmich1991 <kuzmichanna1.22@gmail.com>
2023-12-04 21:15:45 +0300

    Initial commit

---
+++ client/src/main/java/com/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package com.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"org.client","org.core"})
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+}
